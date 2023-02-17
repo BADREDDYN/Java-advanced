d37941bfb77bd9c10723ee587d896e8b7c499ebe
BADREDDYN <badreddynelhirch@gmail.com>
2023-02-17 16:06:42 +0100

    ValueOf.

---
+++ src/Strings/ValueOf.java
@@ -0,0 +1,12 @@
+package Strings;
+public class ValueOf {
+
+    public static void main(String[] args) {
+
+        int n = 20;
+        String s = String.valueOf(n);
+        System.out.println(s + 10); // 2010
+
+    }
+
+}
