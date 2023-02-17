5c88df443d0b38a4d3365f0c6a83afdf189b968a
BADREDDYN <badreddynelhirch@gmail.com>
2023-02-17 15:21:58 +0100

    SubString.

---
+++ src/Strings/SubString.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class SubString {
+    public static void main(String[] args) {
+
+        String s1 = "JAVA PROGRAMMING LANGUAGE";
+        System.out.println(s1.substring(5, 16)); // PROGRAMMING
+        System.out.println(s1.substring(0)); // JAVA PROGRAMMING LANGUAGE
+        System.out.println(s1.substring(5)); // PROGRAMMING LANGUAGE
+
+    }
+
+}
